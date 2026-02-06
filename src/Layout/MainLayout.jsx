6ad74c292fa7730e79475c086406831781fb4f2a
Hasib Al Mamun <hasib.gub221@gmail.com>
2026-02-06 23:49:35 +0600

    adding navigation bar and footer

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
+import { Outlet } from 'react-router';
+
+const MainLayout = () => {
+    return (
+        <div>
+          <Navbar/>
+
+          <div className='mt-4'>
+            <Outlet/>
+          </div>
+
+          <Footer/>
+        </div>
+    );
+};
+
+export default MainLayout;
