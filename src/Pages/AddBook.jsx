955e0176a84a8bcd7da326a0a922af29840b01d3
Hasib Al Mamun <hasib.gub221@gmail.com>
2026-02-25 00:18:43 +0600

    adding addbook page with post api

--- src/Pages/AddBook.jsx
+++ src/Pages/AddBook.jsx
@@ -1,15 +1,44 @@
 // pages/AddBook.jsx
-import React from "react";
+import React, { useContext } from "react";
+import AuthContext from "../Context/AuthContext";
 
 export default function AddBook() {
+  const {user } = useContext(AuthContext);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = {
+      title: e.target.name.value,
+      genre: e.target.category.value,
+      summary: e.target.description.value,
+      coverImage: e.target.coverImage.value,
+      userEmail: user?.email || "Unknown",
+      rating : 0,
+      author: user?.name || "Unknown"
+
+    };
+
+    fetch('http://localhost:3000/allbooks',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log("Book added successfully:", data);  
+    } )
+    .catch(error => {
+      console.error("Error adding book:", error);
+    });
+  }
   return (
     <section className="min-h-screen  py-20">
       <div className="mx-auto max-w-3xl px-6">
         {/* Header */}
         <div className="mb-12 text-center">
-          <h1 className="mb-3 text-4xl font-bold ">
-            Add a New Book
-          </h1>
+          <h1 className="mb-3 text-4xl font-bold ">Add a New Book</h1>
           <p className="text-lg">
             Fill in the details below to add a new book to The Book Haven.
           </p>
@@ -17,7 +46,7 @@ export default function AddBook() {
 
         {/* Form Card */}
         <div className="rounded-3xl  p-10 shadow-xl">
-          <form className="space-y-6">
+          <form onSubmit={handleSubmit} className="space-y-6">
             {/* Book Name */}
             <div>
               <label className="mb-2 block text-sm font-medium ">
@@ -25,6 +54,7 @@ export default function AddBook() {
               </label>
               <input
                 type="text"
+                name="name"
                 placeholder="Enter book name"
                 className="w-full rounded-xl border border-gray-300 px-4 py-3 text-sm focus:border-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-100"
               />
@@ -32,16 +62,19 @@ export default function AddBook() {
 
             {/* Category */}
             <div>
-              <label className="mb-2 block text-sm font-medium ">
-                Category
-              </label>
-              <select className="w-full rounded-xl border border-gray-300 px-4 py-3 text-sm focus:border-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-100">
+              <label className="mb-2 block text-sm font-medium">Category</label>
+
+              <select
+                name="category"
+                required
+                className="w-full rounded-xl border border-gray-300 px-4 py-3 text-sm focus:border-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-100"
+              >
                 <option value="">Select a category</option>
-                <option>Fiction</option>
-                <option>Non-fiction</option>
-                <option>Mystery</option>
-                <option>Romance</option>
-                <option>Fantasy</option>
+                <option value="Fiction">Fiction</option>
+                <option value="Non-fiction">Non-fiction</option>
+                <option value="Mystery">Mystery</option>
+                <option value="Romance">Romance</option>
+                <option value="Fantasy">Fantasy</option>
               </select>
             </div>
 
@@ -50,8 +83,9 @@ export default function AddBook() {
               <label className="mb-2 block text-sm font-medium ">
                 Description
               </label>
-              <textarea
-                rows="4"
+              <input
+                type="text"
+                name="description"
                 placeholder="Write a short description about the book"
                 className="w-full resize-none rounded-xl border border-gray-300 px-4 py-3 text-sm focus:border-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-100"
               />
@@ -63,6 +97,7 @@ export default function AddBook() {
                 Photo URL
               </label>
               <input
+                name="coverImage"
                 type="url"
                 placeholder="https://example.com/book-cover.jpg"
                 className="w-full rounded-xl border border-gray-300 px-4 py-3 text-sm focus:border-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-100"
